80398d81bead55b90c1e38ff080826feb9ea1e1c
Ashley <106272446+Ashleyxbrianna@users.noreply.github.com>
2022-08-30 14:00:53 -0700

    adding definition

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,21 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState ("");
+    let [results, setResults] = useState (null);
 
 
     function handleResponse(response){
-        console.log(response.data[0])
+        setResults(response.data[0]);
     }
     
     
     function search(event){
         event.preventDefault();
-        alert (`Searching for ${keyword}`)
+        
 
 
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
@@ -33,7 +35,8 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChanage}/>
                 </form>
+                <Results results={results}/>
             
         </div>
-    )
+    );
 }
