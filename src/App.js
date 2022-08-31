0ff3d9592f71387ccca05c53c97a39ee4da43a61
Ashley <106272446+Ashleyxbrianna@users.noreply.github.com>
2022-08-31 12:08:17 -0700

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@
 import './App.css';
 import Dictionary from './Dictionary';
 
+
+
+
 function App() {
   return (
     <div className="App">
       <div className="container">
+        
       
       <header className="App-header">
+       
+      
       </header>
+        
       <main>
+        
         <Dictionary defaultKeyword="Sword"/>
       </main>
-      <footer className="App-footer">Coded by Ashley Brianna</footer>
+      <footer className="App-footer">Coded by Ashley Brianna <a href='https://github.com/Ashleyxbrianna/dictionary-project' target="_blank" rel='noreferrer'>Open sourced</a></footer>
     </div>
     </div>  );
 }
